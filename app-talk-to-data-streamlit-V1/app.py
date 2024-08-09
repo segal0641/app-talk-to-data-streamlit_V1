358b069128217ae8d73dd32863f41d45630a0b93
Adenis Segal <adenis.segal@inovally.com.br>
2024-08-09 10:52:02 -0300

    primeiro commit

---
+++ app-talk-to-data-streamlit-V1/app.py
@@ -0,0 +1,92 @@
+import os
+import streamlit as st
+import pandas as pd
+import dotenv
+from dotenv import load_dotenv
+import pymongo
+
+from pandasai import Agent
+from pandasai.llm.openai import OpenAI
+from deep_translator import GoogleTranslator
+import matplotlib
+
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+matplotlib.use('Agg')
+
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+#openai_api_key = st.secrets["OPENAI_API_KEY"]
+#pandas_ai_api_key = st.secrets["PANDAS_AI_API_KEY"]
+#db_user = st.secrets["DB_USER"]
+#db_password = st.secrets["DB_PASSWORD"]
+#db_host = st.secrets["DB_HOST"]
+
+
+openai_api_key = os.environ["OPENAI_API_KEY"]
+pandas_ai_api_key = os.environ["PANDAS_AI_API_KEY"]
+db_user = os.environ["DB_USER"]
+db_password = os.environ["DB_PASSWORD"]
+db_host = os.environ["DB_HOST"]
+llm = OpenAI(api_token=openai_api_key)
+
+
+def extract_transform_data():
+    client_hml = pymongo.MongoClient('mongodb+srv://'+db_user+':'+db_password+'@data-api-homolog.rzzzqv5.mongodb.net', unicode_decode_error_handler='ignore')
+    
+    db = client_hml['analytics']
+    collection = db["score_devedores"]
+    df = pd.json_normalize(list(collection.find()))
+
+    df_final = df.drop(columns=['DOCUMENTO', 'PT_TOTAL', 'PT_SOMA'])
+
+    return df_final
+
+df = extract_transform_data()
+
+st.set_page_config(page_title="App Talk to Data", page_icon=":bar_chart:", layout="wide")
+#logo_path = 'https://raw.githubusercontent.com/adenissegall/app-talk-to-data-streamlit/main/app-talk-to-data-streamlit/imagens/logo_inovally.png'
+logo_path = './imagens/logo_inovally.png'
+link_inovally = "https://inovally.com.br/"
+
+#logo_url = 'imagens/logo_inovally.png'
+#st.logo("./logo_inovally.png", link=link_inovally)
+st.logo(logo_path, link=link_inovally)
+
+
+#st.title("App Talk to Data")
+st.markdown("<h1 style='text-align: center; color: #14D2AA;'>App Talk to Data</h1>", unsafe_allow_html=True)
+
+
+st.subheader("Use o poder da IA Generativa para descobrir insights sobre os devedores do seu município! 📊📈")
+#imagem_url = 'https://raw.githubusercontent.com/adenissegall/app-talk-to-data-streamlit/main/app-talk-to-data-streamlit/imagens/image.png'
+imagem_url = "./imagens/image.png"
+st.image(imagem_url)
+#st.image("./image.png")
+
+
+dados = Agent(df, config={"llm": llm, "enable_cache": False})
+
+
+st.dataframe(df)
+
+
+st.subheader("No espaço abaixo, escreva o que você gostaria de ver nos dados 👇")
+texto_usuario = st.text_area("Ex: Qual é o nome do contribuinte com maior montante financeiro? e qual é o montante financeiro dele? Me mostre um gráfico de pizza com o percentual de cada categoria para TAG_SCORE. Uma cor diferente para cada categoria")
+texto_usuario = texto_usuario + ', responda em português'
+texto_usuario_traduzido = GoogleTranslator(source='auto', target='en').translate(texto_usuario)
+texto_usuario_traduzido = texto_usuario_traduzido.replace('\u200b\u200b', '')
+
+
+if st.button("Gerar resultado"):
+    if texto_usuario_traduzido:
+        with st.spinner("Gerando resultado..."):
+            answer = dados.chat(texto_usuario_traduzido)
+            st.success(answer)
+
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            st.pyplot()
+    else:
+        st.warning("Por favor, informe o que você quer ver nos dados.")
+    
+
+
