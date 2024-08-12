a6178144a06adde3012851bec6886f9b1931b4c8
Adenis Segal <adenis.segal@inovally.com.br>
2024-08-12 09:42:12 -0300

    alterando plot grafico4

--- app-talk-to-data-streamlit-V1/app.py
+++ app-talk-to-data-streamlit-V1/app.py
@@ -117,13 +117,7 @@ class StreamlitResponse(ResponseParser):
 
 if st.button("Gerar resultado"):
     if texto_usuario_traduzido:
-        llm = OpenAI(api_token=os.environ["OPENAI_API_KEY"])
-        query_engine = SmartDataframe(
-            df,
-            config={
-                "llm": llm,
-                "response_parser": StreamlitResponse
-            },
-        )
-
+        llm = OpenAI(api_token=openai_api_key)
+        query_engine = SmartDataframe(df, config={"llm": llm, "response_parser": StreamlitResponse})
         answer = query_engine.chat(texto_usuario_traduzido)
+        st.write(answer)
