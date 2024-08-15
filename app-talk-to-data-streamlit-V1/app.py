6a0c3fa65a412cc310d4bf62dc624f1932b8c940
Adenis Segal <adenis.segal@inovally.com.br>
2024-08-15 09:42:28 -0300

    alterando exemplos de pergunta 3

--- app-talk-to-data-streamlit-V1/app.py
+++ app-talk-to-data-streamlit-V1/app.py
@@ -69,9 +69,9 @@ st.dataframe(df)
 
 st.subheader("No espaço abaixo, você pode escrever o que gostaria de ver nos dados!")
 st.text("Exemplos:")
-st.text("☑️ Qual é o nome do contribuinte com maior montante financeiro? e qual é o montante financeiro dele?")
-st.text("☑️ Me mostre um gráfico de pizza com o percentual de cada categoria para TAG_SCORE. Uma cor diferente para cada categoria")
-st.text("☑️ Me mostre uma tabela com os nomes, montante financeiro e tag_score  das pessoas que possuem tag_score = Excelente")
+st.write("☑️ Qual é o nome do contribuinte com maior montante financeiro? e qual é o montante financeiro dele?")
+st.write("☑️ Me mostre um gráfico de pizza com o percentual de cada categoria para TAG_SCORE. Uma cor diferente para cada categoria")
+st.write("☑️ Me mostre uma tabela com os nomes, montante financeiro e tag_score  das pessoas que possuem tag_score = Excelente")
 texto_usuario = st.text_area("")
 texto_usuario = texto_usuario + ', responda em português'
 texto_usuario_traduzido = GoogleTranslator(source='auto', target='en').translate(texto_usuario)
